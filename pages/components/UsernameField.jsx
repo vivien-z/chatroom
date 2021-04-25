5f6a42851a90a8d8659e968f505a7cd048706ba5
vivien zhu <yuanzyy@gmail.com>
2021-04-24 21:37:07 -0400

    initial setup

---
+++ pages/components/UsernameField.jsx
@@ -0,0 +1,30 @@
+const UsernameField = ({ value, onChange, onSubmit, completed }) => {
+  if (completed) {
+    // if the user has already claimed a username, display it.
+    return (
+      <div>
+        Chatting as <b>{value}</b>
+      </div>
+    );
+  } else {
+    // if the user hasn't yet claimed a username, let them do so.
+    return (
+      <div>
+        <form onSubmit={(e) => e.preventDefault() || onSubmit(value)}>
+          <label>
+            Set your username:
+            <input
+              type="text"
+              name="username"
+              value={value}
+              onChange={(e) => e.preventDefault() || onChange(e.target.value)}
+            />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
+      </div>
+    );
+  }
+};
+
+export default UsernameField;
--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,189 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+import { useEffect, useState } from "react";
+import { io } from "socket.io-client";
+import UsernameField from "./components/UsernameField";
+import styles from '../styles/Home.module.css';
 
 export default function Home() {
+  // save the socket
+  const [socket, setSocket] = useState(null);
+
+  // Whether the username is set.
+  const [isUsernameConfirmed, setUsernameConfirmed] = useState(false);
+
+  // State for the username.
+  const [username, setUsername] = useState("");
+
+  // State for the form field.
+  const [message, setMessage] = useState("");
+
+  // State for message history.
+  const [history, setHistory] = useState([
+    /*
+    {
+      username: "Santa Claus",
+      message: "Ho ho ho!"
+    }
+    */
+  ]);
+
+  const connectSocket = () => {
+    // prime the server first. yes, this is an extra call and is inefficient.
+    // but we're using NextJS for convenience, so this is a necessary evil.
+    fetch("/api/chat");
+    // after making sure that socket server is primed, connect to it.
+
+    if (!socket) {
+      const newSocket = io();
+
+      // Confirms connection
+      newSocket.on("connect", () => {
+        console.log("Chat app connected");
+      });
+
+      // handles message
+      newSocket.on("message", (msg) => {
+        setHistory((history) => [...history, msg]);
+      });
+
+      // Logs when server disconnects
+      newSocket.on("disconnect", () => {
+        console.warn("WARNING: chat app disconnected");
+      });
+
+      setSocket(() => newSocket);
+    }
+  };
+
+  // The websocket code
+  useEffect(() => {
+    connectSocket();
+  }, []);
+
+  // this method submits the form and sends the message to the server.
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+    if (!socket) {
+      alert("Chatroom not connected yet. Try again in a little bit.");
+      return;
+    }
+
+    // prevent empty submissions
+    if (!message || !isUsernameConfirmed) {
+      return;
+    }
+
+    // submit and blank-out the field.
+    socket.emit("message-submitted", { message, username });
+    setMessage("");
+  };
+
   return (
-    <div className={styles.container}>
+    <div>
+      {/* This sets the page's title and favicon */}
       <Head>
-        <title>Create Next App</title>
+        <title>My Chat App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+      {/* The username area */}
+      <UsernameField
+        completed={isUsernameConfirmed}
+        value={username}
+        onChange={(value) => setUsername(value)}
+        onSubmit={() => setUsernameConfirmed(true)}
+      />
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+      {/* Form submission */}
+      <div>
+        <form onSubmit={handleSubmit}>
+          <label>
+            Type your message:
+            <input
+              type="text"
+              name="message"
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+              placeholder={
+                username ? "Enter your message..." : "Set username..."
+              }
+              disabled={!isUsernameConfirmed}
+            />
+          </label>
+          <input type="submit" value="Submit" disabled={!isUsernameConfirmed} />
+        </form>
+      </div>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+      {/* The list of messages */}
+      <div>
+        {history.map(({ username, message }, i) => (
+          <div key={i}>
+            <b>{username}</b>: {message}
+          </div>
+        ))}
+      </div>
     </div>
-  )
+  );
 }
+// export default function Home() {
+//   return (
+//     <div className={styles.container}>
+//       <Head>
+//         <title>Create Next App</title>
+//         <link rel="icon" href="/favicon.ico" />
+//       </Head>
+
+//       <main className={styles.main}>
+//         <h1 className={styles.title}>
+//           Welcome to <a href="https://nextjs.org">Next.js!</a>
+//         </h1>
+
+//         <p className={styles.description}>
+//           Get started by editing{' '}
+//           <code className={styles.code}>pages/index.js</code>
+//         </p>
+
+//         <div className={styles.grid}>
+//           <a href="https://nextjs.org/docs" className={styles.card}>
+//             <h3>Documentation &rarr;</h3>
+//             <p>Find in-depth information about Next.js features and API.</p>
+//           </a>
+
+//           <a href="https://nextjs.org/learn" className={styles.card}>
+//             <h3>Learn &rarr;</h3>
+//             <p>Learn about Next.js in an interactive course with quizzes!</p>
+//           </a>
+
+//           <a
+//             href="https://github.com/vercel/next.js/tree/master/examples"
+//             className={styles.card}
+//           >
+//             <h3>Examples &rarr;</h3>
+//             <p>Discover and deploy boilerplate example Next.js projects.</p>
+//           </a>
+
+//           <a
+//             href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+//             className={styles.card}
+//           >
+//             <h3>Deploy &rarr;</h3>
+//             <p>
+//               Instantly deploy your Next.js site to a public URL with Vercel.
+//             </p>
+//           </a>
+//         </div>
+//       </main>
+
+//       <footer className={styles.footer}>
+//         <a
+//           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Powered by{' '}
+//           <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+//         </a>
+//       </footer>
+//     </div>
+//   )
+// }

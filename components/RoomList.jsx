a199988c2abfaa9451581bc79a0e4ddea8700146
vivien zhu <yuanzyy@gmail.com>
2021-04-29 10:55:00 -0400

    located and fixed chatroom not presenting issue

--- components/RoomList.jsx
+++ components/RoomList.jsx
@@ -2,12 +2,13 @@ import styles from '../styles/RoomList.module.css';
 
 const RoomList = ({ value }) => {
 
+console.log(value)
   return (
     <div className={styles.chatroomList}>
       <ul>
-        {value.map(({ chatroom }, i) => (
+        {value.map((chatroom, i) => (
           <li key={i}>
-            chatroom
+            {`Room: ${chatroom}`}
           </li>
         ))}
       </ul>
@@ -16,20 +17,3 @@ const RoomList = ({ value }) => {
 };
 
 export default RoomList;
-
-    // <div className={styles.chatroomForm}>
-    //   <form  onSubmit={(e) => e.preventDefault() || onSubmit(value)}>
-    //     <label>
-    //       Create a new chatroom:
-    //     </label>
-    //     <input
-    //       className={styles.formInput}
-    //       type="text"
-    //       name="chatroom"
-    //       value={value}
-    //       onChange={(e) => e.preventDefault() || onChange(e.target.value)}
-    //       placeholder={placeholder}
-    //     />
-    //     <input className={styles.formButton} type="submit" value="Add" />
-    //   </form>
-    // </div>

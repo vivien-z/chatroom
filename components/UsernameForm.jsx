1d1ce52e2db62733e1a1918991189386f6e73cdc
vivien zhu <yuanzyy@gmail.com>
2021-07-28 15:55:35 -0400

    added sass to app. set primary color to red, secondary color to beige

--- components/UsernameForm.jsx
+++ components/UsernameForm.jsx
@@ -4,6 +4,8 @@ import { useUsers } from "../context/UsersProvider";
 import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import InputGroup from 'react-bootstrap/Button';
+import styles from '../styles/UsernameForm.module.scss';
 
 const UsernameForm = ({ value, onChange, disabled, onUsernameSubmit }) => {
   const idRef = useRef()
@@ -32,36 +34,39 @@ const UsernameForm = ({ value, onChange, disabled, onUsernameSubmit }) => {
 
   if (!disabled) {
     return (
-      <Container className="d-flex justify-content-center m-5">
+      <Container className="d-flex justify-content-center my-5">
         <Form
-          className="w-50 p-3 py-5 border"
+          className={`${styles.form} w-50 border bg-white rounded`}
           onSubmit={ handleSubmit }
           id="new-username"
         >
-          <Form.Group>
-            <Form.Label>
-              Id:
-            </Form.Label>
-            <Form.Control
-              type="text"
-              name="user-id"
-              ref={idRef}
-              required
-            />
-            <Form.Label>
-              Set an username:
-            </Form.Label>
-            <Form.Control
-              type="text"
-              name="username"
-              ref={usernameRef}
-              value={value}
-              onChange={(e) => e.preventDefault() || onChange(e.target.value)}
-              required
-            />
-          </Form.Group>
-          <Button onClick={setRandomUsername} variant="secondary">Random usernames</Button>
-          <Button type="submit">Confirm</Button>
+          <div className={styles.formTitle}>Welcome to the community</div>
+          <div className="p-4">
+            <Form.Group>
+              <Form.Label><strong>Id</strong></Form.Label>
+              <Form.Control
+                className="mb-3"
+                type="text"
+                name="user-id"
+                ref={idRef}
+                required
+              />
+              <Form.Label><strong>Username</strong></Form.Label>
+              <div className="input-group mb-3">
+                <Form.Control
+                  type="text"
+                  name="username"
+                  ref={usernameRef}
+                  value={value}
+                  onChange={(e) => e.preventDefault() || onChange(e.target.value)}
+                  required
+                />
+                <Button onClick={setRandomUsername} variant="secondary">Random usernames</Button>
+              </div>
+            </Form.Group>
+            <Button className="btn btn-primary me-2" type="submit">New User Sign Up</Button>
+            <Button className="btn btn-primary" type="submit">Login</Button>
+          </div>
         </Form>
       </Container>
     );

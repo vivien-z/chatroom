0cf4c50dbb43b5c9ed03d02ac4e732b7d8b4321f
vivien zhu <yuanzyy@gmail.com>
2021-07-28 22:03:06 -0400

    add style for the sidebar list

--- components/Chatrooms.jsx
+++ components/Chatrooms.jsx
@@ -18,27 +18,11 @@ const Chatrooms = ({ value }) => {
           active={chatroom.selected}
         >
           <b>{chatroom.roomname}</b>
-
           <ListGroup className={chatroom.selected ? null : 'd-none'}>
             {chatroom.roomUsers.map((roomUser, i) =>
               <ListGroup.Item key={i}>{roomUser.username}</ListGroup.Item>
             )}
           </ListGroup>
-
-{/*          <Dropdown>
-            <Dropdown.Toggle variant="success" id="chatroom-users">
-              <b>{chatroom.roomname}</b>
-            </Dropdown.Toggle>
-            <Dropdown.Menu>
-              <ListGroup show={chatroom.selected}>
-                {chatroom.roomUsers.map((roomUser, i) =>
-                  <Dropdown.Item href={`#/action-{i}`}>
-                    <ListGroup.Item key={i}>{roomUser.name}</ListGroup.Item>
-                  </Dropdown.Item>
-                )}
-              </ListGroup>
-            </Dropdown.Menu>
-          </Dropdown>*/}
 
         </ListGroup.Item>
       ))}
